709369ee8e74f0a700e3d91bc35997cdcd8b0cdb
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-15 20:11:27 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,7 +9,7 @@ class ArtistsController < ApplicationController
   end 
   
   def new
-    @artist = Artist.new(artist_params(:name, :bio))
+    @artist = Artist.new
   end 
   
   def create
@@ -20,7 +20,7 @@ class ArtistsController < ApplicationController
 
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(artist_params(:name, :bio))
+    @artist.update(artist_params(:name))
     redirect_to artist_path(@artist)
   end
 
