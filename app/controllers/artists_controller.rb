3d8cf77b2ff74957de72485a15d930260ddd1aa3
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-15 20:13:29 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,7 +13,7 @@ class ArtistsController < ApplicationController
   end 
   
   def create
-    @artist = Artist.new(params)
+    @artist = Artist.new(artist_params(:name))
     @artist.save
     redirect_to artist_path(@artist)
   end
